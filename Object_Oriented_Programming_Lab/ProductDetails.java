aa594e3bec7d1f2b03c33903b7ba1c9e5ceb9751
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-06 21:17:06 +0530

    Update ProductDetails.java

--- Object_Oriented_Programming_Lab/ProductDetails.java
+++ Object_Oriented_Programming_Lab/ProductDetails.java
@@ -4,13 +4,13 @@ class product
     String pcode, pname;
     int price;
     product(String pcode, String pname, int price) 
-	{
+    {
         this.pcode = pcode;
         this.pname = pname;
         this.price = price;
     }
     public void display() 
-	{
+    {
         System.out.println("Product code : " + this.pcode);
         System.out.println("Product name : " + this.pname);
         System.out.println("Product price : " + this.price);
@@ -20,11 +20,11 @@ class product
 public class ProductDetails 
 {
     public static void main(String args[]) 
-	{
+    {
         String code, name;
         int cost;
 		
-		//System.out.print("Enter the number of products: ");
+	//System.out.print("Enter the number of products: ");
         //int n = sc.nextInt();
         //sc.nextLine(); 
         //product p[] = new product[n];
@@ -46,17 +46,17 @@ public class ProductDetails
         }
 
         if (p[0].price < p[1].price && p[0].price < p[2].price) 
-		{
+	{
             System.out.println("Product 1 has lowest price");
             p[0].display();
         } 
-		else if (p[1].price < p[0].price && p[1].price < p[2].price) 
-		{
+	else if (p[1].price < p[0].price && p[1].price < p[2].price) 
+	{
             System.out.println("Product 2 has lowest price");
             p[1].display();
         } 
-		else 
-		{
+	else 
+	{
             System.out.println("Product 3 has lowest price");
             p[2].display();
         }
