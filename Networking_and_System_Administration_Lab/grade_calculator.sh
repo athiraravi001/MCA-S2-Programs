e4ae8f171d08d96e130b2f93d1d42e947fc00a28
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-09 21:16:31 +0530

    Add files via upload

---
+++ Networking_and_System_Administration_Lab/grade_calculator.sh
@@ -0,0 +1,21 @@
+read -p "Enter marks obtained : " marks_obtained
+read -p "Enter total marks : " total_marks
+
+percentage=$(( marks_obtained * 100 / total_marks ))
+echo "Percentage : $percentage%"
+
+if [ "$percentage" -ge 90 ]
+then
+    echo "Grade : A"
+elif [ "$percentage" -ge 80 ]
+then
+    echo "Grade : B"
+elif [ "$percentage" -ge 70 ]
+then
+    echo "Grade : C"
+elif [ "$percentage" -ge 60 ]
+then
+    echo "Grade : D"
+else
+    echo "Grade : Failed"
+fi
