7d226765e62ada406427f6bab49e74843ad204ae
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-12 21:55:42 +0530

    Add files via upload

---
+++ Networking_and_System_Administration_Lab/calculator.sh
@@ -0,0 +1,46 @@
+echo "Enter first number : "
+read num1
+
+echo "Enter second number : "
+read num2
+
+while true;
+do
+    echo ""
+    echo "Choose an operation :-"
+    echo "1 : Addition"
+    echo "2 : Subtraction"
+    echo "3 : Multiplication"
+    echo "4 : Division"
+    echo "5 : Exit"
+    
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1)  echo "***** Addition *****"
+            result=`expr $num1 + $num2`
+            echo "Result = $result"
+            ;;
+        2)  echo "***** Subtraction *****"
+            result=`expr $num1 - $num2`
+            echo "Result = $result"
+            ;;
+        3)  echo "***** Multiplication *****"
+            result=`expr $num1 \* $num2`
+            echo "Result = $result"
+            ;;
+        4)  echo "***** Division *****"
+            if [ "$num2" -ne 0 ]; then
+                result=`echo "scale=2; $num1 / $num2" | bc`
+                echo "Result = $result"
+            else
+                echo "Error: Division by zero"
+            fi
+            ;;
+        5)   echo "Exited"
+            exit 0
+            ;;
+        *)  echo "Invalid choice"
+            ;;
+    esac
+done
