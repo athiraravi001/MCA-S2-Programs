7668d605febfc4c15347cc818596122cdc8d0799
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-14 22:21:05 +0530

    Add files via upload

---
+++ Advanced_DatabaseManagementSystem_Lab/parity_check_with_userdefined_exception.sql
@@ -0,0 +1,17 @@
+declare
+    n integer := &n;
+    a exception;
+    b exception;
+begin
+    if mod(n, 2) = 0 then
+        raise a;
+    else
+        raise b;
+    end if;
+exception
+    when a then
+        dbms_output.put_line('The input is even');
+    when b then
+        dbms_output.put_line('The input is odd');
+end;
+/
