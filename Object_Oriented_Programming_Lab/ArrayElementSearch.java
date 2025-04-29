7625281418ae75ceccb4611353413961e613e8ac
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-29 20:36:17 +0530

    Add files via upload

---
+++ Object_Oriented_Programming_Lab/ArrayElementSearch.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class ArrayElementSearch
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array size : ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.print("Enter the array elements : ");
+        for (int i = 0; i < n; i++) 
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element to be searched : ");
+        int key = sc.nextInt();
+
+        boolean flag = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == key)
+            {
+                flag = true;
+                break;
+            }
+        }
+        
+        if (flag)
+        {
+            System.out.print("Element is found");
+        }
+        else 
+        {
+            System.out.print("Element is not found");
+        }
+        sc.close();
+    }
+}
