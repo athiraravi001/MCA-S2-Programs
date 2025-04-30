fac835f27b9e3a1811596b9f8c6926409ffd73b5
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-04-30 22:30:58 +0530

    Update StringManipulations.java

--- Object_Oriented_Programming_Lab/StringManipulations.java
+++ Object_Oriented_Programming_Lab/StringManipulations.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class StringManipulations
 {
-	public static void main(String arg[])
+	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the first string : ");
