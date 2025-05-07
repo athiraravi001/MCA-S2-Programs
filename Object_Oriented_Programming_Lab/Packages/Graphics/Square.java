6a78c3631bc93d5fa4da4c2d8aef5bc4e5696e50
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-05-07 21:16:12 +0530

    Add files via upload

---
+++ Object_Oriented_Programming_Lab/Packages/Graphics/Square.java
@@ -0,0 +1,15 @@
+package Graphics;
+import java.util.Scanner;
+public class Square implements Shapes
+{
+    int side;
+    public Square(int side)
+    {
+        this.side = side;
+    }
+    public void area()
+    {
+        int area = this.side * this.side;
+        System.out.print("Area of square = "+area);
+    }
+}
