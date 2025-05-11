9da0f03ea5f4500518258d4a27f594d835f176a2
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-05-11 11:51:34 +0530

    Add files via upload

--- Object_Oriented_Programming_Lab/ComputerConfiguration.java
+++ Object_Oriented_Programming_Lab/ComputerConfiguration.java
@@ -55,35 +55,37 @@ class CPU
 {
     double price;
     CPU(double price) 
-	{
+    {
         this.price = price;
     }
+	
     class Processor 
-	{
+    {
         int cores;
         String manufacturer;
         Processor(int cores, String manufacturer) 
-		{
+	{
             this.cores = cores;
             this.manufacturer = manufacturer;
         }
         void displayProcessorInfo() 
-		{
+	{
             System.out.println("Processor Cores: " + cores);
             System.out.println("Processor Manufacturer: " + manufacturer);
         }
     }
+	
     static class RAM 
-	{
+    {
         int memory;
         String manufacturer;
         RAM(int memory, String manufacturer) 
-		{
+	{
             this.memory = memory;
             this.manufacturer = manufacturer;
         }
         void displayRAMInfo()
-		{
+	{
             System.out.println("RAM Memory: " + memory + "GB");
             System.out.println("RAM Manufacturer: " + manufacturer);
         }
@@ -93,7 +95,7 @@ class CPU
 public class ComputerConfiguration 
 {
     public static void main(String[] args) 
-	{
+    {
         CPU cpu = new CPU(5000.0);
         System.out.println("CPU Price: " + cpu.price);
 
@@ -101,6 +103,6 @@ public class ComputerConfiguration
         processor.displayProcessorInfo();
 
         CPU.RAM ram = new CPU.RAM(16, "Samsung");
-       ram.displayRAMInfo();
+        ram.displayRAMInfo();
     }
 }
