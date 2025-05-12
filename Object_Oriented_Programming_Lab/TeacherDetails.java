d394014b3e3d9b3039335cf162196f1b41d671c4
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-05-12 07:35:15 +0530

    Add files via upload

--- Object_Oriented_Programming_Lab/TeacherDetails.java
+++ Object_Oriented_Programming_Lab/TeacherDetails.java
@@ -8,7 +8,7 @@ class Person
 	int age;
 	Person(String name, String gender, String address, int age)
 	{
-	    this.name = name;
+	        this.name = name;
 		this.gender = gender;
 		this.address = address;
 		this.age = age;
