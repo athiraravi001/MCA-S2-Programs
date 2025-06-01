9151f87c6ed7f5102a7d293de84cf5f923654cd9
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-06-01 18:02:01 +0530

    Add files via upload

---
+++ Networking_and_System_Administration_Lab/concatenation_of_two_files.sh
@@ -0,0 +1,36 @@
+echo "Enter first filename : "
+read a
+if [ -f "$a" ]
+then
+  cat "$a"
+else
+  echo "File '$a' not found."
+fi
+
+echo "Enter second filename : "
+read b
+if [ -f "$b" ]
+then
+  cat "$b"
+else
+  echo "File '$b' not found."
+fi
+
+echo "----- After Concatenating -----"
+cat "$a" "$b" > cc
+cat cc
+
+
+#---------------------------------------------------- OR ----------------------------------------------------
+
+echo "Enter first filename"
+read a
+cat "$a"
+
+echo "Enter second filename"
+read b
+cat "$b"
+
+echo "----- After Concatenating -----"
+cat "$a" "$b" > cc
+cat cc
