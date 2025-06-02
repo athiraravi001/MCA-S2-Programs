d2feb52ac18b7bcc630546681de2a0e6b7835e53
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-06-02 12:39:50 +0530

    Update odd_position_digits_of_a_number.sh

--- Networking_and_System_Administration_Lab/odd_position_digits_of_a_number.sh
+++ Networking_and_System_Administration_Lab/odd_position_digits_of_a_number.sh
@@ -1,15 +1,17 @@
-echo "Enter a five digit number: "
+echo "Enter a five digit number : "
 read num
 
-if ! [[ $num =~ ^[0-9]{5}$ ]] 
+if [[ $num =~ ^[0-9]{5}$ ]]
 then
-  echo "Invalid input. Please enter exactly a five-digit number."
-  exit 1
-fi
+  echo "Valid 5-digit number"
 
-n=1
-while [ $n -le 5 ]
-do
-  echo $num | cut -c $n
-  n=$(( n+2 ))
-done
+  n=1
+  while [ $n -le 5 ]
+  do
+    echo "$num" | cut -c $n
+    n=$(( n + 2 ))
+  done
+
+else
+  echo "Invalid input"
+fi
